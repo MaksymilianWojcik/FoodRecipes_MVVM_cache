c5b125555d43a203b8435dbfd5cf06c7f0aff2d5
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-05-11 15:22:05 +0200

    Showing categories

--- app/src/main/java/pl/com/bubka/foodrecipeswithcache/viewmodels/RecipeListViewModel.java
+++ app/src/main/java/pl/com/bubka/foodrecipeswithcache/viewmodels/RecipeListViewModel.java
@@ -3,15 +3,36 @@ package pl.com.bubka.foodrecipeswithcache.viewmodels;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
 
 public class RecipeListViewModel extends AndroidViewModel {
 
     private static final String TAG = "RecipeListViewModel";
 
+
+    public enum ViewState {CATEGORIES, RECIPES}; //should be repaced with static final int later
+
+    private MutableLiveData<ViewState> viewState;
+
+
     public RecipeListViewModel(@NonNull Application application) {
         super(application);
 
+        init();
+
+    }
+
+    private void init() {
+        if(viewState == null){ //nie zostal jeszcze zainicjowany (np. kied peirwszy raz jest stworzony view model
+            viewState = new MutableLiveData<>();
+            viewState.setValue(ViewState.CATEGORIES); //chcemy za pierwszym razem widziec kategorie
+        }
+    }
+
+    public LiveData<ViewState> getViewState(){
+        return viewState;
     }
 
 
